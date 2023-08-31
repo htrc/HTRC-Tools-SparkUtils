ebd02fd33b71cb38a4926f5f85a2dbaa95219410
Boris Capitanu <capitanu@illinois.edu>
2023-08-31 08:20:23 -0500

    Minor update

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,17 +1,11 @@
-import sbt.Keys._
-import sbt._
+import sbt.*
+import sbt.Keys.*
 
 object Dependencies {
 
   def spark(version: String): Seq[Def.Setting[Seq[ModuleID]]] = Seq(
     libraryDependencies ++= Seq(
       "org.apache.spark" %% "spark-core"      % version % Provided
-    )
-  )
-
-  def spark_dev(version: String): Seq[Def.Setting[Seq[ModuleID]]] = Seq(
-    libraryDependencies ++= Seq(
-      "org.apache.spark" %% "spark-core"      % version
     )
   )
 
